16e8023f64580f11e32c8fd4a51ee67c98f71e8c
Anesti Thomarai <11153152@college.wlc.ac.uk>
2023-12-19 11:47:57 +0000

    2rfef

--- FilmQuiz/frmQuestion1.vb
+++ FilmQuiz/frmQuestion1.vb
@@ -1,15 +1,5 @@
 ﻿Public Class btnQuestion
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
-    End Sub
-
-    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
-
-    End Sub
 
     Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
         If btnAnswer2.Checked Then
@@ -19,19 +9,4 @@
         Me.Hide()
     End Sub
 
-    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
-
-    End Sub
-
-    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
-
-    End Sub
-
-    Private Sub btnAnswer2_CheckedChanged(sender As Object, e As EventArgs) Handles btnAnswer2.CheckedChanged
-
-    End Sub
-
-    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
-
-    End Sub
 End Class
